29b59e0e601e6ffe1915ca4e140d36894c97f6ab
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2023-10-20 21:07:38 +0800

    Update A2_A0254474Y.py

--- Assignment 2/A2_A0254474Y.py
+++ Assignment 2/A2_A0254474Y.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Oct 20 17:23:32 2023
 
-@author: clare
+@author: Clarence Poh 
 """
 
 import numpy as np
@@ -39,7 +39,7 @@ def A2_A0254474Y(N):
     iris_dataset = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], 
                                                         test_size=test_size, random_state=random_state)
-    onehot_encoder=OneHotEncoder(sparse=False)
+    onehot_encoder=OneHotEncoder(sparse_output=False)
     
     reshaped_training = y_train.reshape(len(y_train), 1)
     Ytr = onehot_encoder.fit_transform(reshaped_training)
@@ -49,62 +49,45 @@ def A2_A0254474Y(N):
     Ptrain_list = CreateRegressors(X_train, 8)
     Ptest_list = CreateRegressors(X_test, 8)
 
-    w_list = EstimateRegressionCoefficients(Ptrain_list, y_train, reg)
+    w_list = []
+    for Ptrain in Ptrain_list:
+        if Ptrain.shape[0] >= Ptrain.shape[1]:
+            w = inv(Ptrain.T @ Ptrain + reg*np.identity(Ptrain.shape[1])) @ Ptrain.T @ Ytr
+            w_list.append(w)
+        else:
+            w = Ptrain.T @ inv(Ptrain @ Ptrain.T + reg*np.identity(Ptrain.shape[0])) @ Ytr
+            w_list.append(w)
 
     y_training_argmax = []
-    for i in range(0,8):
-        y_training_est_p = Ptrain_list[i] @ w_list[i]
-        y_training_cls_p = [[1 if y == max(x) else 0 for y in x] for x in y_training_est_p]
-        y_training_argmax.append(y_training_cls_p)
-
     y_test_argmax = []
     for i in range(0,8):
+        y_training_est_p = Ptrain_list[i] @ w_list[i]
+        y_training_cls_p = y_training_est_p.argmax(axis=1)
+        y_training_argmax.append(y_training_cls_p)
+
         y_test_est_p = Ptest_list[i] @ w_list[i]
-        y_test_cls_p = [[1 if y == max(x) else 0 for y in x] for x in y_test_est_p]
+        y_test_cls_p = y_test_est_p.argmax(axis=1)
         y_test_argmax.append(y_test_cls_p)
-    
+        
+    error_train_array = []
     for ytrain in y_training_argmax:
-        error_train_array = np.array([sum(ytrain != y_train)])
-            
+        error_train_array.append(sum(ytrain != y_train))
+
+    error_test_array = []
     for ytest in y_test_argmax:
-        error_test_array = np.array([sum(ytest != y_test)])
-    # return in this order
+        error_test_array.append(sum(ytest != y_test))
+        
+    error_train_array = np.array(error_train_array)
+    error_test_array = np.array(error_test_array)
+
+    
     return X_train, y_train, X_test, y_test, Ytr, Yts, Ptrain_list, Ptest_list, w_list, error_train_array, error_test_array
 
 def CreateRegressors(x, max_order):
     P = [] 
 
     for order in range(1, max_order+1):   
-
         P_current_regressors = PolynomialFeatures(order).fit_transform(x)
         P.append(P_current_regressors)
 
     return P
-
-def EstimateRegressionCoefficients(P_list, y, reg=None):
-    # from Note 2: If the number of rows in the training polynomial 
-    #              is less than or equal to the number of columns, 
-    #              use the dual form of ridge regression.
-    #              If not, use the primal form.
-   
-    w_list = [] 
-  
-    if reg is None: # no regularization
-        for P in P_list:
-            if(P.shape[1] > P.shape[0]): # dual form
-                 w = P.T @ inv(P @ P.T) @ y
-            else: # primal form
-                 w = (inv(P.T @ P) @ P.T) @ y
-
-    else: # with regularization
-        for P in P_list:
-            if(P.shape[1] > P.shape[0]): # dual form
-                 w = P.T @ inv(P @ P.T + reg*np.eye(P.shape[0])) @ y
-            else: # primal form
-                 w = ( inv(P.T @ P + reg*np.eye(P.shape[1]) ) @ P.T) @ y 
-            
-    w_list.append(w)
-
-    return w_list
-
-X_train, y_train, X_test, y_test, Ytr, Yts, Ptrain_list, Ptest_list,w_list, error_train_array, error_test_array = A2_A0254474Y(5)
