d9a10910c08e5bf7e1e2651e846c0f93f660abd9
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2023-11-13 15:21:53 +0800

    Added A3 Skeleton Code

---
+++ Assignment 3/A3_A0254474Y.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+# Please replace "StudentMatriculationNumber" with your actual matric number here and in the filename
+def A3_MatricNumber(learning_rate, num_iters):
+    """
+    Input type
+    :learning_rate type: float
+    :num_iters type: int
+
+    Return type
+    :a_out type: numpy array of length num_iters
+    :f1_out type: numpy array of length num_iters
+    :b_out type: numpy array of length num_iters
+    :f2_out type: numpy array of length num_iters
+    :c_out type: numpy array of length num_iters
+    :d_out type: numpy array of length num_iters
+    :f3_out type: numpy array of length num_iters
+    """
+    # your code goes here
+
+
+    # return in this order
+    return a_out, f1_out, b_out, f2_out, c_out, d_out, f3_out 
